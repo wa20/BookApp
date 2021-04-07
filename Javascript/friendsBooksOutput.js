e6697b9415fa0585bff56d9a6140ffc3642171dd
Mbongeni Allan Ngwenya <mbongeniallanngwenya@Mbongenis-MacBook-Pro.local>
2021-04-07 22:22:09 +0100

    logic to output the friends list of books is done, will add more logic so it outputs to the intended friends

---
+++ Javascript/friendsBooksOutput.js
@@ -0,0 +1,93 @@
+//friendsredirect
+const searchBtnEl = document.getElementById("search-btn");
+const searchEl = document.getElementById("search-bar");
+const redirectEl = document.getElementById("redirectEl"); 
+
+const searchBtnNavEl = document.getElementById("search-btn-nav");
+const searchNavEl = document.getElementById("search-bar-nav");
+const redirectNavEl = document.getElementById("redirectNavEl"); 
+
+/* console.log(searchBtnEl);
+console.log(searchEl); */
+const resultsUrl = "./friends_list.html";
+const APIKey = "AIzaSyC2xEWKYLtmXP4EC1KSovcnRSpX9h3NSTs";
+
+var formSubmitHandler = function() {
+/*     event.preventDefault(); */
+    var userInput = searchEl.value;
+    localStorage.setItem("input", JSON.stringify(userInput))
+    if (userInput) {
+        bookSearch(userInput);
+    } else {
+        alert("Please enter your friends book list");
+    }
+};
+
+var formSubmitHandlerNav = function() {
+    /*     event.preventDefault(); */
+        var userInput = searchNavEl.value;
+        localStorage.setItem("input", JSON.stringify(userInput))
+        if (userInput) {
+            bookSearchNav(userInput);
+        } else {
+            alert("Please enter your friends book list");
+        }
+    };
+
+var bookSearch = function(searchedBook) {
+    var requestUrl = "https://www.googleapis.com/books/v1/volumes?q=" + searchedBook + "&maxResults=15" + "&key=" + APIKey;
+    fetch(requestUrl)
+        .then(function(response) {
+            if(response.ok) {
+                /* console.log(response); */
+                response.json().then(function(data) {
+                    /* console.log(data); */
+                    pageRedirect();    
+                });
+            } else {
+                alert('Error: ' + response.statusText)
+            }
+        });
+};
+
+var bookSearchNav = function(searchedBook) {
+    var requestUrl = "https://www.googleapis.com/books/v1/volumes?q=" + searchedBook + "&maxResults=15" + "&key=" + APIKey;
+    fetch(requestUrl)
+        .then(function(response) {
+            if(response.ok) {
+                /* console.log(response); */
+                response.json().then(function(data) {
+                    /* console.log(data); */
+                    pageRedirectNav();    
+                });
+            } else {
+                alert('Error: ' + response.statusText)
+            }
+        });
+};
+
+function pageRedirect() {
+    redirectEl.setAttribute('href', './friends_list.html');
+    searchBtnEl.innerHTML = "Are you sure?";
+    /* console.log(searchBtnEl);   
+    console.log(redirectEl);  */ 
+}
+
+function pageRedirectNav() {
+    redirectNavEl.setAttribute('href', './friends_list.html');
+    /* console.log(searchBtnNavEl);   
+    console.log(redirectNavEl); */  
+}
+
+
+searchBtnEl.addEventListener("click", function(){
+   /*  console.log("hi"); */
+    formSubmitHandler();
+});
+
+searchBtnNavEl.addEventListener("click", function(){
+    /* console.log("hi"); */
+    searchBtnNavEl.classList.remove("search");
+    searchBtnNavEl.setAttribute("class", "arrow right icon");
+    formSubmitHandlerNav();
+})
